e9bdc861d7765aa8c3852c0afe470bfbc55b8a9b
Kim <kim@Pauls-Mac-mini.local>
2025-04-28 02:07:45 +1000

    Add WhiteNoise

--- ngs/settings.py
+++ ngs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g_2&-2_h2v(9z6hdkzntx#5&s9$5lgmfud9gbdr#)-7@w$do&v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['43.139.195.82']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ngs.urls'
@@ -121,6 +122,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'website' / 'static',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
