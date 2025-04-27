fb49779339b611d713a5a983fccf992cf677730c
Kim <kim@Pauls-Mac-mini.local>
2025-04-27 15:54:06 +1000

    gunicorn and whitenoise

--- ngs/settings.py
+++ ngs/settings.py
@@ -48,11 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Added
 ]
 
 ROOT_URLCONF = 'ngs.urls'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  # Added
 
 TEMPLATES = [
     {
