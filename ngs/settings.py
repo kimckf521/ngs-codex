d72691ca8703d98929ae51c0d815405d9136689a
Kim <kim@Pauls-Mac-mini.local>
2025-04-29 18:37:18 +1000

    Change DEBUG = Flase

--- ngs/settings.py
+++ ngs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g_2&-2_h2v(9z6hdkzntx#5&s9$5lgmfud9gbdr#)-7@w$do&v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['43.139.195.82', '127.0.0.1']
 
