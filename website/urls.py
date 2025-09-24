86c5cee0cfc624f462cc807eeeeaab7487d2c34d
kimckf521 <paulckf521@outlook.com>
2025-09-25 05:00:55 +1000

    Up To College Mentorship

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('price/', views.price, name='price'),
-    path('tutor/', views.tutor, name='tutor'),
-    path('why-ngs/', views.why_ngs, name='why-ngs'),
+    path('cclr_programs/', views.cclr_programs, name='cclr_programs'),
+    path('dual_track_learning/', views.dual_track_learning,
+         name='dual_track_learning'),
+    path('hybrid_learning/', views.hybrid_learning, name='hybrid_learning'),
+    path('online_diploma_program/', views.online_diploma_program,
+         name='online_diploma_program'),
+    path('programs/', views.programs,
+         name='programs'),
+    path('college_mentorship/', views.college_mentorship,
+         name='college_mentorship'),
+    path('highschool_mapping/', views.highschool_mapping,
+         name='highschool_mapping'),
+    path('faculty/', views.faculty,
+         name='faculty'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -4,14 +4,44 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'website/index.html')
 
-
-def price(request):
-    return render(request, 'website/price.html')
+# Partner With Us
 
 
-def tutor(request):
-    return render(request, 'website/tutor.html')
+def cclr_programs(request):
+    return render(request, 'website/partner_with_us/cclr_programs.html')
 
 
-def why_ngs(request):
-    return render(request, 'website/why-ngs.html')
+def hybrid_learning(request):
+    return render(request, 'website/partner_with_us/hybrid_learning.html')
+
+
+def dual_track_learning(request):
+    return render(request, 'website/partner_with_us/dual_track_learning.html')
+
+
+def online_diploma_program(request):
+    return render(request, 'website/partner_with_us/online_diploma_program.html')
+
+# Study With Us
+
+
+def programs(request):
+    return render(request, 'website/study_with_us/programs.html')
+
+
+def college_mentorship(request):
+    return render(request, 'website/study_with_us/college_mentorship.html')
+
+
+def highschool_mapping(request):
+    return render(request, 'website/study_with_us/highschool_mapping.html')
+
+
+def faculty(request):
+    return render(request, 'website/study_with_us/faculty.html')
+
+
+# Admissions
+
+
+# Join Ngs Commmunity
